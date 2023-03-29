a5b4963c440ab12bdc949e1663734cacfb40f91e
w1z13 <115799235+w1z13@users.noreply.github.com>
2023-03-29 10:25:24 +0200

    finished

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 const searchField = document.querySelector('#text');
 const searchButton = document.querySelector('#search');
 const content = document.querySelector('.content');
+const loc = document.querySelector('.loc');
 const img = document.querySelector('.img');
 const temp = document.querySelector('.temp');
 const wind = document.querySelector('.wind');
 const humidity = document.querySelector('.humidity');
 
 async function getWeather(search) {
-    let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=1f17ebf852004d2cb54140322232103&q=${search}`, {mode: 'cors'});
-    let data = await response.json();
-    let weather = data.current;
-
-    console.log(data);
-
-    img.src = weather.condition.icon;
-    temp.innerText = `Temp: ${weather.temp_c}℃`;
-    wind.innerText = `Wind: ${weather.wind_kph} km/h ${weather.wind_dir}`;
-    humidity.innerText = `Humidity: ${weather.humidity}%`;
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=1f17ebf852004d2cb54140322232103&q=${search}`, {mode: 'cors'});
+    if (response.ok){
+        searchField.value = '';
+        const data = await response.json();
+        content.style.visibility = 'visible';
+        const weather = data.current;
+        loc.innerText = `Weather in ${data.location.name}, ${data.location.region}, ${data.location.country}:`;
+        img.src = weather.condition.icon;
+        temp.innerText = `Temp: ${weather.temp_c}℃`;
+        wind.innerText = `Wind: ${weather.wind_kph} km/h ${weather.wind_dir}`;
+        humidity.innerText = `Humidity: ${weather.humidity}%`;
+    } else console.log('error', response.status);
 }
+
+searchField.addEventListener('keydown', (e) => {
+    if (e.code === 'Enter'){
+        e.preventDefault();
+        if (searchField.value != '') getWeather(searchField.value);
+    }
+});
 
 searchButton.addEventListener('click', (e) => {
     e.preventDefault;
     getWeather(searchField.value);
 });
-
-
